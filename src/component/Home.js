14c1778c901474615935aaec7131c606dd1748a6
Naveen Dhakad <nthanna8@gmail.com>
2022-10-31 11:42:06 +0530

    done 2

--- src/component/Home.js
+++ src/component/Home.js
@@ -20,7 +20,7 @@ function Home(){
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">
-            Home 2
+            Home
           </a>
         </li>
         <li className="nav-item">
